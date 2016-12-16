a8b27c75d2070dc5bc55ff8138b6b14d12a97b56
Phat Nguyen <pnguyen91@csc.com>
2016-12-16 12:05:26 +0700

    - Style

--- wp-content/themes/kingsmens/search.php
+++ wp-content/themes/kingsmens/search.php
@@ -1,6 +1,6 @@
 <?php get_header(  ) ?>
-<div class="content">
-    <div id="main-content">
+<div class="content page">
+    <div id="index">
         <div class="search-info">
             <?php 
                 $search_query = new WP_Query( 's='.$s.'&showpost=-1' );
@@ -11,7 +11,7 @@
             ?>
         </div>
         <?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-            <?php get_template_part( 'content', get_post_format() ); ?>
+            <?php get_template_part( 'content', 'search' ); ?>
 
         <?php endwhile ?>
         <?php kingsmen_pagination(); ?>
@@ -19,9 +19,6 @@
             <?php get_template_part( 'content', 'none' ); ?>
         <?php endif; ?>
 
-    </div>
-    <div id="sidebar">
-        <?php get_sidebar(); ?>
     </div>
 </div>
 <?php get_footer() ?>
--- wp-content/themes/kingsmens/templates/home.php
+++ wp-content/themes/kingsmens/templates/home.php
@@ -6,10 +6,32 @@ Template Name: Home
 <?php get_header(  ) ?>
 <div class="content">
     <div id="main-content">
-
     <?php 
-echo do_shortcode('[smartslider3 slider=1]');
-?>
+        echo do_shortcode('[smartslider3 slider=1]');
+    ?>
+    </div>
+    <div class="latest-news">
+        <h2>Latest News</h2>
+        <?php
+            $recent_posts = wp_get_recent_posts();
+            foreach( $recent_posts as $recent ) {
+
+                if ($recent === reset($recent_posts))
+                {   
+                    echo '<li>';
+                    echo get_the_post_thumbnail($recent['ID'], 'full');
+                    printf( '<a href="%1$s">%2$s</a></li>',
+                        esc_url( get_permalink( $recent['ID'] ) ),
+                        apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
+                    );
+                }else{
+                    printf( '<li><a href="%1$s">%2$s</a></li>',
+                        esc_url( get_permalink( $recent['ID'] ) ),
+                        apply_filters( 'the_title', $recent['post_title'], $recent['ID'] )
+                    );
+                }
+            }
+        ?>
     </div>
 </div>
 <?php get_footer() ?>
